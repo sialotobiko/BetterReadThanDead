2afd420936a61465504cdb6d591769a8f51d42dd
sialotobiko <sialo.tobiko@student.moringaschool.com>
2022-10-06 01:09:32 +0300

    Adds initial functions

---
+++ src/components/BookList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function BookList(){
+    return (
+        console.log("I will display a list of books in our catalogue")
+    )
+}
+
+export default BookList;
---
+++ src/components/NewBookForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function NewBookForm(){
+    return (
+        console.log("Add your new book here")
+    )
+}
+
+export default NewBookForm;
