50042c06a66f0edc557d92518821976a8932a6a6
sialotobiko <sialo.tobiko@student.moringaschool.com>
2022-10-06 16:32:42 +0300

    Adds some content to the homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,17 @@ import React from "react";
 function Home(){
     return (
         <div className="home">
+            <br/>
+            <br/>
             <h1>Welcome to our book club!</h1>
+            <p>Our book club intends to create a shared reading experience for a community of readers registered as our members.</p>
+            <p>The goal of this project is to encourage home reading experiences.</p>
+            <p>We have a wide range of book titles of different genres in our catalogue.</p> 
+                <p>Each month, members are assigned their book of choice to read for the month.</p>
+               <p> At the end of the month, members grouped by category convene and review the books. </p>
+               <p> This way, you get to read hundreds of books without actually reading them! How interesting!</p>
+             <p>To join our community or donate a book, please send us an email at <em>monthlybookclub@gmail.com</em></p>  
+                
         </div>
     )
 }
