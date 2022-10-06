2bf6de459545edcf5710c6b3728d32721dffd770
sialotobiko <sialo.tobiko@student.moringaschool.com>
2022-10-07 01:54:40 +0300

    Creates a form to enter new book

--- src/components/NewBookForm.js
+++ src/components/NewBookForm.js
@@ -1,9 +1,32 @@
 import React from "react";
 
-function NewBookForm(){
-    return (
-        console.log("Add your new book here")
-    )
+function NewBookForm() {
+  return (
+    <>
+      <h1>Enter details of the new book here</h1>
+      <form className="newbookform">
+        <label>
+          Title
+          <input type="text" name="text" />
+        </label>
+        <label>
+            Category
+            <input type="text" name="text" />
+        </label>
+        <label>
+          Genre 
+          <input type="text" name="text" />
+        </label>
+        <label>
+            Year Published
+            <input type="text" name="text" />
+        </label>
+        <div>
+        <input id="submit" type="submit" value="Add Book"/>
+        </div>
+      </form>
+    </>
+  );
 }
 
 export default NewBookForm;
