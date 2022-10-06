ab7d42b4c0d1fb13840b08ddc035cbe933e064c8
sialotobiko <sialo.tobiko@student.moringaschool.com>
2022-10-07 00:25:46 +0300

    Renders books data in a table

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,8 +1,44 @@
 import React from "react";
+import Book from "./Book";
+import { useEffect, useState } from "react";
 
 function BookList(){
+
+const [booksList, setBooksList] = useState([])
+
+useEffect(() => {
+    fetchBookData()
+}, [])
+
+function fetchBookData(){
+    fetch ("http://localhost:5000/booksData")
+    .then((res) => res.json())
+    .then((res) => setBooksList(res))
+}
+
     return (
-        console.log("I will display a list of books in our catalogue")
+        <div className='books'>
+      {/* display a list of books using Book component */}
+
+      <tbody>
+        <tr>
+            <th>Book ID</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Genre</th>
+            <th>Year Published</th>
+        </tr>
+      </tbody>
+      {booksList.map((book, id) => (
+        <tr key={id}>
+            <td>{book.id}</td>
+            <td>{book.title}</td>
+            <td>{book.author}</td>
+            <td>{book.genre}</td>
+            <td>{book.yearPublished}</td>
+        </tr>
+      ))}
+    </div>
     )
 }
 
