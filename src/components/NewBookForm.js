365d831ce778c3c72fdc33e14a7aa63ca5a7e45f
sialotobiko <sialo.tobiko@student.moringaschool.com>
2022-10-07 17:54:06 +0300

    Posts new data to db.json

--- src/components/NewBookForm.js
+++ src/components/NewBookForm.js
@@ -1,28 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 
 function NewBookForm() {
+  const [books, updateBooks] = useState({
+    title: "",
+    author: "",
+    genre: "",
+    yearPublished: "",
+  });
+
+  function submit(e) {
+    e.preventDefault();
+
+
+    fetch("http://localhost:5000/booksData", {
+      method: "POST",
+      body: JSON.stringify(books),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then(() => updateBooks());
+  }
+
+  function handle(e) {
+    const newData = { ...books };
+    newData[e.target.id] = e.target.value;
+    updateBooks(newData);
+    console.log(newData);
+  }
+
   return (
     <>
       <h1>Enter details of the new book here</h1>
-      <form className="newbookform">
+      <form className="newbookform" onSubmit={(e) => submit(e)}>
         <label>
           Title
-          <input type="text" name="text" />
+          <input
+            type="text"
+            name="title"
+            id="title"
+            onChange={(e) => handle(e)}
+            value={books.title}
+          />
         </label>
         <label>
-            Category
-            <input type="text" name="text" />
+          Author
+          <input
+            type="text"
+            name="author"
+            id="author"
+            onChange={(e) => handle(e)}
+            value={books.author}
+          />
         </label>
         <label>
-          Genre 
-          <input type="text" name="text" />
+          Genre
+          <input
+            type="text"
+            name="genre"
+            id="genre"
+            onChange={(e) => handle(e)}
+            value={books.genre}
+          />
         </label>
         <label>
-            Year Published
-            <input type="text" name="text" />
+          Year Published
+          <input
+            type="text"
+            name="year"
+            id="year"
+            onChange={(e) => handle(e)}
+            value={books.yearPublished}
+          />
         </label>
         <div>
-        <input id="submit" type="submit" value="Add Book"/>
+          <input id="submit" type="submit" value="Add Book" />
         </div>
       </form>
     </>
